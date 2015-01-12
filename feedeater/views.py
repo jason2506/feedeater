c7dde9ac682c3f1ac85f5335857a40df6a19d220
Chi-En Wu <jason2506.mail@gmail.com>
2015-01-12 23:14:10 +0800

    Implement pagination

--- feedeater/views.py
+++ feedeater/views.py
@@ -2,13 +2,17 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from flask import Blueprint, url_for, abort, redirect, render_template
+from flask import Blueprint, url_for, request, abort, redirect, render_template
+from sqlalchemy.sql.expression import func
 
 from .models import Session, Item, Feed, Source
+from .pagination import Pagination
 
 module = Blueprint('feedeater', __name__,
                    template_folder='templates',
                    static_folder='static')
+
+_PER_PAGE = 10
 
 
 @module.app_template_filter('datetime')
@@ -19,6 +23,13 @@ def _jinja2_filter_datetime(date, fmt=None):
     return date.strftime(fmt)
 
 
+@module.app_template_global('url_for_page')
+def _jinja2_url_for_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+
 @module.route('/')
 def index():
     return redirect(url_for('.show_sources'))
@@ -26,8 +37,11 @@ def index():
 
 @module.route('/sources')
 def show_sources():
-    sources = Session.query(Source).all()
-    return render_template('show_sources.html', sources=sources)
+    page = request.args.get('page', default=1, type=int)
+    count = Session.query(func.count(Source.id)).scalar()
+    sources = Session.query(Source).slice(_PER_PAGE * (page - 1), _PER_PAGE * page)
+    pagination = Pagination(page, _PER_PAGE, count)
+    return render_template('show_sources.html', sources=sources, pagination=pagination)
 
 
 @module.route('/sources/<int:source_id>/edit')
@@ -47,22 +61,25 @@ def delete_source(source_id):
 
 @module.route('/sources/<int:source_id>/feeds')
 def show_feeds(source_id):
-    source = Session.query(Source.name).filter_by(id=source_id).one()
-    if source is None:
+    source_name = Session.query(Source.name).filter_by(id=source_id).scalar()
+    if source_name is None:
         abort(404)
 
-    feeds = Session.query(Feed).filter_by(source_id=source_id)
+    page = request.args.get('page', default=1, type=int)
+    count = Session.query(func.count(Feed.id)).filter_by(source_id=source_id).scalar()
+    feeds = Session.query(Feed).filter_by(source_id=source_id).slice(_PER_PAGE * (page - 1), _PER_PAGE * page)
+    pagination = Pagination(page, _PER_PAGE, count)
 
     breadcrumb = (
-        (source.name, url_for('.show_feeds', source_id=source_id)),
+        (source_name, url_for('.show_feeds', source_id=source_id)),
     )
-    return render_template('show_feeds.html', feeds=feeds, breadcrumb=breadcrumb)
+    return render_template('show_feeds.html', feeds=feeds, pagination=pagination, breadcrumb=breadcrumb)
 
 
 @module.route('/sources/<int:source_id>/feeds/<int:feed_id>/edit')
 def edit_feed(source_id, feed_id):
-    source = Session.query(Source.name).filter_by(id=source_id).one()
-    if source is None:
+    source_name = Session.query(Source.name).filter_by(id=source_id).scalar()
+    if source_name is None:
         abort(404)
 
     feed = Session.query(Feed).filter_by(id=feed_id, source_id=source_id).one()
@@ -70,7 +87,7 @@ def edit_feed(source_id, feed_id):
         abort(404)
 
     breadcrumb = (
-        (source.name, url_for('.show_feeds', source_id=source_id)),
+        (source_name, url_for('.show_feeds', source_id=source_id)),
     )
     return render_template('edit_feed.html', feed=feed, breadcrumb=breadcrumb)
 
@@ -83,18 +100,21 @@ def delete_feed(source_id, feed_id):
 
 @module.route('/sources/<int:source_id>/feeds/<int:feed_id>/items')
 def show_items(source_id, feed_id):
-    source = Session.query(Source.name).filter_by(id=source_id).one()
-    if source is None:
+    source_name = Session.query(Source.name).filter_by(id=source_id).scalar()
+    if source_name is None:
         abort(404)
 
-    feed = Session.query(Feed.name).filter_by(id=feed_id, source_id=source_id).one()
-    if feed is None:
+    feed_name = Session.query(Feed.name).filter_by(id=feed_id, source_id=source_id).scalar()
+    if feed_name is None:
         abort(404)
 
-    items = Session.query(Item).filter_by(feed_id=feed_id).order_by(Item.crawled_at.desc())
+    page = request.args.get('page', default=1, type=int)
+    count = Session.query(func.count(Item.id)).filter_by(feed_id=feed_id).scalar()
+    items = Session.query(Item).filter_by(feed_id=feed_id).order_by(Item.crawled_at.desc()).slice(_PER_PAGE * (page - 1), _PER_PAGE * page)
+    pagination = Pagination(page, _PER_PAGE, count)
 
     breadcrumb = (
-        (source.name, url_for('.show_feeds', source_id=source_id)),
-        (feed.name, url_for('.show_items', source_id=source_id, feed_id=feed_id)),
+        (source_name, url_for('.show_feeds', source_id=source_id)),
+        (feed_name, url_for('.show_items', source_id=source_id, feed_id=feed_id)),
     )
-    return render_template('show_items.html', items=items, breadcrumb=breadcrumb)
+    return render_template('show_items.html', items=items, pagination=pagination, breadcrumb=breadcrumb)
